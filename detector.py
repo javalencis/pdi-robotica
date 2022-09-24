d7e72a1c4ed661267d83e492b15a4913503ad737
Jonathan Valencia <javalencis@gmail.com>
2022-09-24 11:17:26 -0500

    Punto del medio

--- detector.py
+++ detector.py
@@ -42,6 +42,7 @@ while True:
             if (M["m00"] == 0): M["m00"]=1
             x = int(M["m10"]/M["m00"]) #Getting the x coordinate
             y = int((M["m01"] / M["m00"])) 
+            cv2.circle(frame, (x,y), 7, (0,255,0), -1) 
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(frame, '{},{}'.format(x,y),(x+50,y), font, 0.75,(0,255,0),1,cv2.LINE_AA)#Mostrar ubicación en la pantalla
 
@@ -68,7 +69,12 @@ while True:
         area = cv2.contourArea(c)
         if area > 2000:
             x, y, w, h = cv2.boundingRect(c)
-
+            M = cv2.moments(c)
+            if (M["m00"] == 0): M["m00"]=1
+            x = int(M["m10"]/M["m00"]) #Getting the x coordinate
+            y = int((M["m01"] / M["m00"])) 
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, '{},{}'.format(x,y),(x,y), font, 0.75,(0,255,0),1,cv2.LINE_AA)
             cv2.putText(frame, ' Verde',(x,y-5),1,1.2,(0,255,0),2)
 
     cv2.imshow('frame', frame)
